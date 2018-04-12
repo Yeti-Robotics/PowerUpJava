d5c4565cade898b2fcff57940580ac7d9ecc5743
Jared Wagler <jared.wagler@gmail.com>
2018-04-11 22:07:13 -0400

    Put an auto chooser in

--- src/org/usfirst/frc/team3506/robot/Robot.java
+++ src/org/usfirst/frc/team3506/robot/Robot.java
@@ -7,6 +7,10 @@
 
 package org.usfirst.frc.team3506.robot;
 
+import org.usfirst.frc.team3506.robot.Robot.AutoModes;
+import org.usfirst.frc.team3506.robot.commands.autonomous.CrossDefenseAutonomous;
+import org.usfirst.frc.team3506.robot.commands.commandgroups.ChevalDeFriseBreacherCommandGroup;
+import org.usfirst.frc.team3506.robot.commands.commandgroups.LowBarBreacherCommandGroup;
 import org.usfirst.frc.team3506.robot.subsystems.ClampCubeSubsystem;
 import org.usfirst.frc.team3506.robot.subsystems.DrivetrainSubsystem;
 import org.usfirst.frc.team3506.robot.subsystems.ElevatorSubsystem;
@@ -16,8 +20,14 @@ import org.usfirst.frc.team3506.robot.subsystems.RightDrivetrainSubsystem;
 import org.usfirst.frc.team3506.robot.subsystems.ShiftGearsSubsystem;
 import org.usfirst.frc.team3506.robot.subsystems.WristSubsystem;
 
-import autoCommands.DriveStraightPIDCommand;
 import autoRoutines.CenterSwitchLeftAutonomous;
+import autoRoutines.CenterSwitchRightAutonomous;
+import autoRoutines.DriveForwardAutonomous;
+import autoRoutines.LeftSideLeftScaleAutonomous;
+import autoRoutines.LeftSideRightScaleAutonomous;
+import autoRoutines.RightSideLeftScaleAutonomous;
+import autoRoutines.RightSideRightScaleAutonomous;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
@@ -25,14 +35,13 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * This program goes out to Daddy Dean and Daddy Andy from AndyMark
- * ez mani $$$
+ * This program goes out to Daddy Dean and Daddy Andy from AndyMark ez mani $$$
  * also cheezy poofs 435
-*/
+ */
 public class Robot extends TimedRobot {
 	public static OI oi;
-	
-	//Instantiates the subsystems
+
+	// Instantiates the subsystems
 	public static LeftDrivetrainSubsystem leftDrivetrainSubsystem;
 	public static RightDrivetrainSubsystem rightDrivetrainSubsystem;
 	public static DrivetrainSubsystem drivetrainSubsystem;
@@ -41,16 +50,19 @@ public class Robot extends TimedRobot {
 	public static WristSubsystem wristSubsystem;
 	public static ShiftGearsSubsystem shiftGearsSubsystem;
 	public static ClampCubeSubsystem clampCubeSubsystem;
-	
+
 	public SendableChooser autoChooser;
-	
-	//TODO: Auto
+
+	public static enum AutoModes {
+		DRIVE_FORWARD, CENTER_SWITCH, LEFT_SCALE, RIGHT_SCALE
+	};
+
+	// TODO: Auto
 	Command autonomousCommand;
-	SendableChooser<Command> m_chooser = new SendableChooser<>();
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
@@ -61,16 +73,21 @@ public class Robot extends TimedRobot {
 		flywheelSubsystem = new FlywheelSubsystem();
 		wristSubsystem = new WristSubsystem();
 		shiftGearsSubsystem = new ShiftGearsSubsystem();
-		clampCubeSubsystem  = new ClampCubeSubsystem();
+		clampCubeSubsystem = new ClampCubeSubsystem();
 		oi = new OI();
+		autoChooser = new SendableChooser();
 		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", m_chooser);
+		autoChooser.addDefault("Drive forward", AutoModes.DRIVE_FORWARD);
+		autoChooser.addObject("Center switch", AutoModes.CENTER_SWITCH);
+		autoChooser.addObject("Left side scale auto", AutoModes.LEFT_SCALE);
+		autoChooser.addObject("Right side scale auto", AutoModes.RIGHT_SCALE);
+		SmartDashboard.putData("Auto Chooser", autoChooser);
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -84,28 +101,59 @@ public class Robot extends TimedRobot {
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * <p>
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
-		autonomousCommand = new CenterSwitchLeftAutonomous();
+		String gameData;
+		gameData = DriverStation.getInstance().getGameSpecificMessage(); //gets game data
+		
+		switch ((AutoModes) autoChooser.getSelected()) {
+		case CENTER_SWITCH:
+			if (gameData.charAt(0) == 'L') {
+				autonomousCommand = new CenterSwitchLeftAutonomous();
+			}
+			else {
+				autonomousCommand = new CenterSwitchRightAutonomous(); //TODO: Create this auto
+			}
+			break;
+		case LEFT_SCALE:
+			if (gameData.charAt(1) == 'L') {
+				autonomousCommand = new LeftSideLeftScaleAutonomous(); //TODO: Create this auto
+			}
+			else {
+				autonomousCommand = new LeftSideRightScaleAutonomous(); //TODO: Create this auto
+			}
+			break;
+		case RIGHT_SCALE:
+			if (gameData.charAt(1) == 'L') {
+				autonomousCommand = new RightSideLeftScaleAutonomous(); //TODO: Create this auto
+			}
+			else {
+				autonomousCommand = new RightSideRightScaleAutonomous(); //TODO: Create this auto
+			}
+			break;
+		default:
+			autonomousCommand = new DriveForwardAutonomous();
+		}
 
 		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
+		 * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		 * switch(autoSelected) { case "My Auto": autonomousCommand = new
+		 * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+		 * ExampleCommand(); break; }
 		 */
 
 		// schedule the autonomous command (example)
-		if (autonomousCommand != null) { 
+		if (autonomousCommand != null) {
 			autonomousCommand.start();
 		}
 	}
