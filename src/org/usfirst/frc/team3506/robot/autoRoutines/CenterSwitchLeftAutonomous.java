9fcf99efb61b04975f74a85daae2b35a69b954c3
Jared Wagler <jared.wagler@gmail.com>
2018-04-16 12:01:30 -0400

    Didn't end up committing the auto changes before so doing that now.

--- src/org/usfirst/frc/team3506/robot/autoRoutines/CenterSwitchLeftAutonomous.java
+++ src/org/usfirst/frc/team3506/robot/autoRoutines/CenterSwitchLeftAutonomous.java
@@ -19,7 +19,7 @@ public class CenterSwitchLeftAutonomous extends CommandGroup {
     	addSequential(new DriveTrainLowShift());
     	addSequential(new CloseClawCommand());
         addSequential(new DriveForDistanceCommand(16, 0, .5));
-        addSequential(new DriveStraightCommand(88, .5));
+        addSequential(new DriveStraightCommand(82, .60));
         addSequential(new DriveForDistanceCommand(16, .5, 0));
         addSequential(new MoveWristTimeCommand(-.5, .5));
         addSequential(new OpenClawCommand()); //Should drop first cube? no.
@@ -27,14 +27,14 @@ public class CenterSwitchLeftAutonomous extends CommandGroup {
         addSequential(new DriveForDistanceCommand(12, -.5, 0));
         addSequential(new DriveStraightCommand(84, -.5));
         addSequential(new DriveForDistanceCommand(18, 0, -.5)); //"don't touch it, i swear!"
-        addSequential(new DriveStraightCommand(70, .5));
+        addSequential(new DriveStraightCommand(64, .60));
         addSequential(new CloseClawCommand());
     	addSequential(new DriveStraightCommand(24, -.5));
     	addSequential(new DriveForDistanceCommand(13, 0, .5));//"ez, ez auto"
-    	addParallel(new DriveStraightCommand(56, .5));
+    	addParallel(new DriveStraightCommand(54, .5));
     	addSequential(new MoveWristTimeCommand(.55, 1.25));
     	addSequential(new WaitCommand(.5));
-    	addSequential(new DriveForDistanceCommand(2, .5, 0));
+    	addSequential(new DriveForDistanceCommand(6, .5, 0));
     	addSequential(new OpenClawCommand());
     }
 }
